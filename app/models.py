d8b89993b97b94dc8409316d08c00146b653c14b
Moshik-laptop <moshiktm1994@gmail.com>
2023-04-11 18:12:43 +0300

    migrate

--- app/models.py
+++ app/models.py
@@ -5,8 +5,21 @@ from django.db import models
 class Search(models.Model):
     input=models.TextField(max_length=200)
 
-# class Country(models.Model):
+class Country(models.Model):
+    country_name=models.CharField(max_length=50)
 
-# class City(models.Model):
+class City(models.Model):
+    country=models.ForeignKey(Country,on_delete=models.CASCADE)
+    city_name=models.CharField(max_length=50)
 
-# class Attractions(models.Model):
+class Attractions(models.Model):
+    city=models.ForeignKey(City,on_delete=models.CASCADE)
+    attraction_name=models.CharField(max_length=50)
+    price=models.SmallIntegerField(null=True)
+
+class QueryChatGPT(models.Model):
+    country_name=models.CharField(max_length=50)
+    during_trip=models.SmallIntegerField()
+    max_stop=models.SmallIntegerField()
+    traveler=models.SmallIntegerField()
+    budget=models.SmallIntegerField()
--- gpt_app/settings.py
+++ gpt_app/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'gpt_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dreamtrip_mhc6', 
+        'USER': 'moshik', 
+        'PASSWORD': 'aHOn2XVjQGdBne1yUMSs5X6SvrD1GOKt',
+        'HOST': 'dpg-cgpu7kheuhlq287lbmpg-a.frankfurt-postgres.render.com', 
+        'PORT': '5432',
     }
 }
 
