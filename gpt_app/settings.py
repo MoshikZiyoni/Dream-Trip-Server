726cff2d43ecf0e3bd8f200d959b1091f32bc522
Moshik-laptop <moshiktm1994@gmail.com>
2023-04-13 21:40:10 +0300

    fix key

--- gpt_app/settings.py
+++ gpt_app/settings.py
@@ -153,7 +153,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ORIGIN = '*'
+CORS_ALLOW_ORIGIN = ['*']
 CORS_ALLOW_CREDENTIALS = True
 
 
