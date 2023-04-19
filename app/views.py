c8307768c95bd3cecaf4b37b99d93f1e4ad4e4f7
Moshik-laptop <moshiktm1994@gmail.com>
2023-04-19 12:59:27 +0300

    fix catch

--- app/views.py
+++ app/views.py
@@ -31,7 +31,7 @@ def gpt_func(request):
 
     # Load your API key from an environment variable or secret management service
     openai.api_key = os.environ.get('API_KEY')
-    # openai.api_key = 
+    # openai.api_key =  
     model_id = 'gpt-3.5-turbo'
 
     def ChatGPT_conversation(conversation):
@@ -43,54 +43,48 @@ def gpt_func(request):
         conversation.append({'role': response.choices[0].message.role, 'content': response.choices[0].message.content})
         return conversation 
     
-    def retry(func, max_attempts=3, sleep_time=3):
-        for attempt in range(max_attempts):
-            try:
-                return func()
-            except Exception as e:
-                print(f"Attempt {attempt+1} failed with error: {e}")
-                time.sleep(sleep_time)
-        raise Exception(f"Function call failed after {max_attempts} attempts")
-    
     conversation = []
     conversation.append({'role': 'system', 'content': 'How may I help you?'})
-    conversation = retry(lambda: ChatGPT_conversation(conversation), max_attempts=3, sleep_time=1)
+    conversation = ( ChatGPT_conversation(conversation))
 
     # print('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
-    try:
-        prompt = (ourmessage)
-        conversation.append({'role': 'user', 'content': prompt})
-        conversation = retry(lambda: ChatGPT_conversation(conversation), max_attempts=3, sleep_time=1)
-        # our_answer=('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
-        our_answer = (conversation[-1]['content'].strip())
-        print ('our_answer first try')
-    except Exception as e:
-        print ('cant GPT')
-        print ((conversation[-1]['content'].strip()),'our_answer ERROR')  
-        traceback.print_exc()
-        print (e)
-        return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    max_attempts = 3
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            prompt = (ourmessage)
+            conversation.append({'role': 'user', 'content': prompt})
+            conversation = ( ChatGPT_conversation(conversation))
+            # our_answer=('{0}: {1}\n'.format(conversation[-1]['role'].strip(), conversation[-1]['content'].strip()))
+            our_answer = (conversation[-1]['content'].strip())
+            print ('our_answer first try')
+        except Exception as e:
+            print ('cant GPT')
+            print ((conversation[-1]['content'].strip()),'our_answer ERROR')  
+            traceback.print_exc()
+            print (e)
+            return Response("Internal Server Error", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        try:
+            start = our_answer.index("```") + 3 # Add 3 to skip over the opening ```
+            end = our_answer.rindex("```") # Find the index of the closing ```
+            json_str = our_answer[start:end] # Extract the JSON string
+        except:
+            print ('json_str',json_str)
+            our_answer=json_str
+            print ("not found '''  the symbol",our_answer)
+        # print('number 1',json_str)
+        try:
+            json_str= json_str.replace('json', '')
+            # print('Success',json_str)
+            query.answer = json_str
+            query.save()
+            print (json_str,'last try')
+            return(Response(json_str))
+        except:
+            # print ('not possible')
+            query.answer = json_str
+            query.save()
+            print ('excpet json_str',json_str)
+            return (Response(json_str))
     
-    try:
-        start = our_answer.index("```") + 3 # Add 3 to skip over the opening ```
-        end = our_answer.rindex("```") # Find the index of the closing ```
-        json_str = our_answer[start:end] # Extract the JSON string
-    except:
-        print ('json_str',json_str)
-        our_answer=json_str
-        print ("not found '''  the symbol",our_answer)
-    # print('number 1',json_str)
-    try:
-        json_str= json_str.replace('json', '')
-        # print('Success',json_str)
-        query.answer = json_str
-        query.save()
-        print (json_str,'last try')
-        return(Response(json_str))
-    except:
-        # print ('not possible')
-        query.answer = json_str
-        query.save()
-        print ('excpet json_str',json_str)
-        return (Response(json_str))
-   
