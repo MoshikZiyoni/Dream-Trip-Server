9e0398be76f61cd1b81dfef98ee0c0518bfb7def
Moshik-laptop <moshiktm1994@gmail.com>
2023-04-22 17:37:40 +0300

    fix something

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from app.task3 import run_gpt_app
 @api_view(['GET', 'POST'])
 def gpt_view(request):
-    question2='{"country":"..","cities":[{"city":,"description":,"attractions":["name":]["descrpition":],"travelDay":}]}'
+    question2='{"country":"..","cities":[{"city":,"description":,"attractions":["name":]["descrpition":],"travelDay":INT}]}'
     ourmessage=f"provide me a Trip to {request.data['mainland']} ,for {request.data['travelers']} trip,budget {request.data['budget']} {request.data['durring']},put the answer in the following JSON structure {question2}"
     result = run_gpt_app(ourmessage)
     return JsonResponse(result,safe=False)
