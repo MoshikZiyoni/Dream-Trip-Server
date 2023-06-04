d0e075df1f98c0d3e0abc6aa200e59d0e3c381e3
Moshik-laptop <moshiktm1994@gmail.com>
2023-06-04 19:07:49 +0300

    add landmarks

--- app/chat.py
+++ app/chat.py
@@ -3,8 +3,8 @@ import os
 import time
 import openai
 from dotenv import load_dotenv
-
-from app.models import QueryChatGPT
+import requests
+from app.models import QueryChatGPT,City
 
 # Load environment variables from .env file
 load_dotenv()
@@ -24,9 +24,46 @@ def run_long_poll_async(ourmessage, retries=3, delay=1):
                     {"role": "user", "content": ourmessage}
                 ]
             )
+            city_list =[]
             answer1 = completion.choices[0].message.content
+            # data = json.loads(answer1)
+            # # Extract all city names
+            # cities = [city['city'] for city in data['cities']]
+            # for city in cities:
+            #     city_list.append(city)
+            # api_key = os.environ.get('GPS_KEY')
+
+            # for city in city_list:
+            #     geocoding_url = f'https://api.opencagedata.com/geocode/v1/json?q={city}&key={api_key}'
+
+            #     response = requests.get(geocoding_url)
+            #     data = response.json()
+
+            #     if data['results']:
+            #         latitude1 = data['results'][0]['geometry']['lat']
+            #         longitude1 = data['results'][0]['geometry']['lng']
+            #         existing_city = City.objects.filter(city=city).first()
+
+            #         if not existing_city:
+            #             city_query = City(city=city, latitude=latitude1, longitude=longitude1)
+            #             city_query.save()
+            #             print(f"City: {city} - Latitude: {latitude1}, Longitude: {longitude1}")
+            #         else:
+            #             print(f"City: {city} - Already exists in the database")
+            #     else:
+            #         print(f"City: {city} - Coordinates not found")
             query = QueryChatGPT(question=ourmessage, answer=answer1)
-            query.save()            
+            query.save()
+            # city_data_gpt = {}
+            # for city in city_list:
+            #     city_object = City.objects.filter(city=city).first()
+            #     if city_object:
+            #         city_data_gpt[city] = {
+            #             'latitude': city_object.latitude,
+            #             'longitude': city_object.longitude
+            #         }
+            #     else:
+            #         print(f"City data not found for: {city}")            
             return answer1
         except openai.error.APIError as e:
             if e.status_code == 429:
--- app/models.py
+++ app/models.py
@@ -10,4 +10,11 @@ class QueryChatGPT(models.Model):
     question = models.CharField(max_length=500, default="")
     answer = models.TextField(null=False, default="")
 
+class City(models.Model):
+    city=models.CharField(max_length=100)
+    latitude=models.FloatField(null=False)
+    longitude=models.FloatField(null=False)
+
+
+
 
