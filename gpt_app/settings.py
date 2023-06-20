d453b59d31fa2db7b9bef9cf34af8da8776abed1
Moshik-laptop <moshiktm1994@gmail.com>
2023-06-20 14:40:23 +0300

    change to posgresql

--- gpt_app/settings.py
+++ gpt_app/settings.py
@@ -132,10 +132,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Dream-Trip',
-        # 'HOST':'localhost',
+        'HOST':'localhost',
         'USER': 'Dream-Trip-New',
         'PASSWORD': 'DreamTrip1!',
-        # 'PORT':'3306',
+        'PORT':'5432',
     },
 }
 
