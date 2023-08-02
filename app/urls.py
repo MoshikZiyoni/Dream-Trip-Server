a0713868bab4a5fb151e7e08653e72a99eb9ccda
Moshik-laptop <moshiktm1994@gmail.com>
2023-08-02 12:06:46 +0300

    add image for country

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,5 @@ app_name = 'app'
 urlpatterns = [
     path("", views.gpt_view, name= "gpt_view"),
     path("popular/",views.popular_country,name='popular_country'),
-    path("  ",views.make_short_trip,name="make_short_trip"),
+    path("search/",views.make_short_trip,name="make_short_trip"),
 ]
