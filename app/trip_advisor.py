129ca9eae050c39b7e7b460aa12cbcb39e7d8951
Moshik-laptop <moshiktm1994@gmail.com>
2024-04-09 13:19:16 +0300

    change the hotels reuslts

--- app/trip_advisor.py
+++ app/trip_advisor.py
@@ -43,53 +43,92 @@ def flickr_api(name,latitude,longitude):
 api_key_foursqaure=os.environ.get('FOURSQUARE')
 
 def foursquare_restaurant(landmarks):
-
     url = "https://api.foursquare.com/v3/places/search?"
+    try:
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key_foursqaure+'='
+        }
+        query= {
+            # 'query': f"attractions in {city_name},{country}",
+            'categories':'13000',
+            "ll" :  f"{landmarks[0]},{landmarks[1]}",
+            'radius':5000,
+            'limit' : 20,
+            'fields':'geocodes,name,rating,price,website,photos,social_media,menu,hours,location,tel,tastes,tips,categories'
+        }
+        response = requests.get(url, params=query,headers=headers)
 
-    headers = {
-        "accept": "application/json",
-        "Authorization": api_key_foursqaure+'='
-    }
-    query= {
-        # 'query': f"attractions in {city_name},{country}",
-        'categories':'13000',
-        "ll" :  f"{landmarks[0]},{landmarks[1]}",
-        'radius':5000,
-        'limit' : 20,
-        'fields':'geocodes,name,rating,price,website,photos,social_media,menu,hours,location,tel,tastes,tips,categories'
-    }
-    response = requests.get(url, params=query,headers=headers)
+        response_text=(response.text)
+        jsonto=json.loads(response_text)
+        reslut=jsonto['results']
+        return (reslut)
+    except:
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key_foursqaure
+        }
+        query= {
+            # 'query': f"attractions in {city_name},{country}",
+            'categories':'13000',
+            "ll" :  f"{landmarks[0]},{landmarks[1]}",
+            'radius':5000,
+            'limit' : 20,
+            'fields':'geocodes,name,rating,price,website,photos,social_media,menu,hours,location,tel,tastes,tips,categories'
+        }
+        response = requests.get(url, params=query,headers=headers)
 
-    response_text=(response.text)
-    jsonto=json.loads(response_text)
-    reslut=jsonto['results']
-    return (reslut)
+        response_text=(response.text)
+        jsonto=json.loads(response_text)
+        reslut=jsonto['results']
+        return (reslut)
 
 def foursquare_attraction(landmarks,city_name,country):
     url1 = "https://api.foursquare.com/v3/places/search?"
+    try:
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key_foursqaure+'='
+        }
 
-    headers = {
-        "accept": "application/json",
-        "Authorization": api_key_foursqaure+'='
-    }
+        query1= {
+            'query': f"attractions in {city_name},{country}",
+            'categories':'10027,10025,10055,10068,16000',
+            "ll" :  f"{landmarks[0]},{landmarks[1]}",
+            'radius':8000,
+            'limit' : 20,
+            'fields':'description,geocodes,name,rating,distance,website,photos,menu,hours_popular,hours,location,tel,tips'
 
-    query1= {
-        'query': f"attractions in {city_name},{country}",
-        'categories':'10027,10025,10055,10068,16000',
-        "ll" :  f"{landmarks[0]},{landmarks[1]}",
-        'radius':8000,
-        'limit' : 20,
-        'fields':'description,geocodes,name,rating,distance,website,photos,menu,hours_popular,hours,location,tel,tips'
+        }
+        response1 = requests.get(url1, params=query1,headers=headers)
 
-    }
-    response1 = requests.get(url1, params=query1,headers=headers)
+        response_text1=(response1.text)
+        jsonto1=json.loads(response_text1)
+        reslut=jsonto1['results']
+        # print('Attracion reslut!!!!!! : ',reslut)
+        return reslut
+    except:
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key_foursqaure
+        }
 
-    response_text1=(response1.text)
-    jsonto1=json.loads(response_text1)
-    reslut=jsonto1['results']
-    # print('Attracion reslut!!!!!! : ',reslut)
-    return reslut
+        query1= {
+            'query': f"attractions in {city_name},{country}",
+            'categories':'10027,10025,10055,10068,16000',
+            "ll" :  f"{landmarks[0]},{landmarks[1]}",
+            'radius':8000,
+            'limit' : 20,
+            'fields':'description,geocodes,name,rating,distance,website,photos,menu,hours_popular,hours,location,tel,tips'
 
+        }
+        response1 = requests.get(url1, params=query1,headers=headers)
+
+        response_text1=(response1.text)
+        jsonto1=json.loads(response_text1)
+        reslut=jsonto1['results']
+        # print('Attracion reslut!!!!!! : ',reslut)
+        return reslut
 
 def foursquare_hotels(landmarks):
     try:
@@ -99,7 +138,6 @@ def foursquare_hotels(landmarks):
             "accept": "application/json",
             "Authorization": api_key_foursqaure+'='
         }
-        print(api_key_foursqaure,'@@@@@@@@')
         query= {
             'categories':'19014',
             "ll" :  f"{landmarks[0]},{landmarks[1]}",
@@ -111,7 +149,6 @@ def foursquare_hotels(landmarks):
         response = requests.get(url, params=query,headers=headers,timeout=5)
         
         response_text=(response.text)
-        print('hotel response @@@@@@@@@@@@@@@@@@@@@@@@',response_text,'@@@@@@@@@@@@@@@@@@@@@@@@')
         jsonto=json.loads(response_text)
         reslut=jsonto['results']
         print (reslut)
@@ -123,7 +160,6 @@ def foursquare_hotels(landmarks):
             "accept": "application/json",
             "Authorization": api_key_foursqaure
         }
-        print(api_key_foursqaure,'@@@@@@@@')
         query= {
             'categories':'19014',
             "ll" :  f"{landmarks[0]},{landmarks[1]}",
@@ -135,7 +171,6 @@ def foursquare_hotels(landmarks):
         response = requests.get(url, params=query,headers=headers,timeout=5)
         
         response_text=(response.text)
-        print('hotel response @@@@@@@@@@@@@@@@@@@@@@@@',response_text,'@@@@@@@@@@@@@@@@@@@@@@@@')
         jsonto=json.loads(response_text)
         reslut=jsonto['results']
         print (reslut)
@@ -145,25 +180,44 @@ def foursquare_hotels(landmarks):
 
 def foursquare_night_life(landmarks):
     url = "https://api.foursquare.com/v3/places/search?"
+    try:
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key_foursqaure+'='
+        }
+        query= {
+            'categories':'10032',
+            "ll" :  f"{landmarks[0]},{landmarks[1]}",
+            'radius':5000,
+            'limit' : 20,
+            
+            'fields':'geocodes,name,rating,website,photos,hours,location,tel,description,tips'
+        }
+        response = requests.get(url, params=query,headers=headers,timeout=5)
 
-    headers = {
-        "accept": "application/json",
-        "Authorization": api_key_foursqaure+'='
-    }
-    query= {
-        'categories':'10032',
-        "ll" :  f"{landmarks[0]},{landmarks[1]}",
-        'radius':5000,
-        'limit' : 20,
-        
-        'fields':'geocodes,name,rating,website,photos,hours,location,tel,description,tips'
-    }
-    response = requests.get(url, params=query,headers=headers,timeout=5)
+        response_text=(response.text)
+        jsonto=json.loads(response_text)
+        reslut=jsonto['results']
+        return (reslut)
+    except:
+        headers = {
+            "accept": "application/json",
+            "Authorization": api_key_foursqaure
+        }
+        query= {
+            'categories':'10032',
+            "ll" :  f"{landmarks[0]},{landmarks[1]}",
+            'radius':5000,
+            'limit' : 20,
+            
+            'fields':'geocodes,name,rating,website,photos,hours,location,tel,description,tips'
+        }
+        response = requests.get(url, params=query,headers=headers,timeout=5)
 
-    response_text=(response.text)
-    jsonto=json.loads(response_text)
-    reslut=jsonto['results']
-    return (reslut)
+        response_text=(response.text)
+        jsonto=json.loads(response_text)
+        reslut=jsonto['results']
+        return (reslut)
 
 
 def my_night_life(landmarks):
